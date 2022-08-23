1867c23552a9a79f34b4c7f1627f5aa6a4ffb049
rama11 <prof.agastyo@gmail.com>
2022-08-24 01:45:42 +0700

    add model updated

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,5 +13,9 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
+        '/barang*',
+        '/pelanggan*',
+        '/item_penjualan*',
+        '/penjualan*'
     ];
 }
