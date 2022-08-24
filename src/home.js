3c500ff1c074f47131cfaadbed64081b924e4c13
rama11 <prof.agastyo@gmail.com>
2022-08-25 02:22:32 +0700

    test add new file 2

--- src/home.js
+++ src/home.js
@@ -3,7 +3,7 @@ import { Outlet, Link} from "react-router-dom";
 
 const Home = () => (
 	<div className="main">
-		<h2 className="main-header">React Crud Operations 2</h2>
+		<h2 className="main-header">React Crud Operations 3</h2>
 	</div>
 )
 
