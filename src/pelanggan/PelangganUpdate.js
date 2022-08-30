8628db7b51b7d5e087bdcc534c43fd44d85dc3e1
rama11 <prof.agastyo@gmail.com>
2022-08-30 09:47:16 +0700

    add PelangganUpdate

---
+++ src/pelanggan/PelangganUpdate.js
@@ -0,0 +1,47 @@
+import React, { useEffect,useState } from 'react';
+import { Button, Checkbox,Table,Form } from 'semantic-ui-react'
+import axios from 'axios'
+
+export default function PelangganUpdate () {
+
+    const [nama, setNama] = useState('');
+    const [domisili, setDomisili] = useState('');
+    const [jenis_kelamin, setJenisKelamin] = useState(false);
+
+    const [id, setIDPelanggan] = useState(null);
+
+    useEffect(() => {
+            setIDPelanggan(localStorage.getItem('ID Pelanggan'))
+            setNama(localStorage.getItem('Nama'));
+            setDomisili(localStorage.getItem('Domisili'));
+            setJenisKelamin(localStorage.getItem('Jenis Kelamin'))
+    }, []);
+
+    const updateAPIData = () => {
+        axios.put(`https://mini-project-laravel.herokuapp.com/pelanggan/${id}`, {
+            nama,
+            domisili,
+            jenis_kelamin
+        })
+    }
+
+    return (
+        <div className="main">
+            <Form className="create-form">
+                <Form.Field>
+                    <label>Nama</label>
+                    <input value={nama} onChange={(e) => setNama(e.target.value)}/>
+                </Form.Field>
+                <Form.Field>
+                    <label>Domisili</label>
+                    <input value={domisili} onChange={(e) => setDomisili(e.target.value)}/>
+                </Form.Field>
+                <Form.Field>
+                    <label>Jenis Kelamin</label>
+                    <input value={jenis_kelamin} onChange={(e) => setJenisKelamin(e.target.value)}/>
+                </Form.Field>
+                <Button type='submit' onClick={updateAPIData}>Update</Button>
+            </Form>
+        </div>
+    )
+}
