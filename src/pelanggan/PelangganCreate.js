1af750415ce85cd8a3befac315fccb61e77911bd
rama11 <prof.agastyo@gmail.com>
2022-08-30 09:47:06 +0700

    add PelangganCreate

---
+++ src/pelanggan/PelangganCreate.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { Button, Checkbox, Form } from 'semantic-ui-react'
+import axios from 'axios'
+
+export default function PelangganCreate() {
+    const [nama, setName] = useState('');
+    const [domisili, setDomisili] = useState('');
+    const [jenis_kelamin, setJenisKelamin] = useState('');
+
+    const postData = () => {
+        axios.post(`https://mini-project-laravel.herokuapp.com/pelanggan`, {
+            nama,
+            domisili,
+            jenis_kelamin
+        })
+	}
+
+    return (
+    	<div className="main">
+	        <div>
+	            <Form className="create-form">
+	                <Form.Field>
+	                    <label>Nama</label>
+	                    <input placeholder='Budi' onChange={(e) => setName(e.target.value)} />
+	                </Form.Field>
+	                <Form.Field>
+	                    <label>Domisili</label>
+	                    <input placeholder='JAK-SEL' onChange={(e) => setDomisili(e.target.value)} />
+	                </Form.Field>
+	                <Form.Field>
+	                    <label>Jenis Kelamin</label>
+	                    <input placeholder='PRIA' onChange={(e) => setJenisKelamin(e.target.value)} />
+	                </Form.Field>
+	                <Button onClick={postData} type='submit'>Submit</Button>
+	            </Form>
+	        </div>
+		</div>
+    )
+}
